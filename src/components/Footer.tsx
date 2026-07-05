ff51731c252609f5752051152f6841d05a305c89
channyhong520-ux <channyhong520@gmail.com>
2026-07-05 20:58:25 +0700

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <div className="px-4 pt-8 pb-4 text-center">
       <p className="text-xs font-medium text-slate-500">
-        Powered by <span className="font-semibold">HangPopok's Penh Smart App</span>
+        Powered by <span className="font-semibold">T4 DEVELOPMENT</span>
       </p>
     </div>
   );
